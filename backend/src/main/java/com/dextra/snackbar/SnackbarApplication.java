5f58263b8b13522c59752ce2f73983eeb90c6a44
Allan Monteiro de Lima <allan_mlpe@hotmail.com>
2020-07-31 01:06:29 -0300

    Enabling CORS

--- backend/src/main/java/com/dextra/snackbar/SnackbarApplication.java
+++ backend/src/main/java/com/dextra/snackbar/SnackbarApplication.java
@@ -4,6 +4,8 @@ import com.dextra.snackbar.model.discount.DiscountCalculatorManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SnackbarApplication {
@@ -11,6 +13,16 @@ public class SnackbarApplication {
 	@Bean
 	public DiscountCalculatorManager discountCalculatorManager() {
 		return new DiscountCalculatorManager();
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
 	}
 
 	public static void main(String[] args) {
